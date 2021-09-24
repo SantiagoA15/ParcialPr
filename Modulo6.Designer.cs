31e789839f0c8174252fdf02a7a80f31c9f31e1b
SantiagoA15 <sac2001_@hotmail.com>
2021-09-24 09:20:37 -0500

    Modulo 6

---
+++ Modulo6.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace ParcialPr
+{
+    partial class Modulo6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ParcialPr.Properties.Resources.iniciar;
+            this.pictureBox3.Location = new System.Drawing.Point(97, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(194, 46);
+            this.pictureBox3.TabIndex = 27;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Adivina la palabra";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(577, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Perdiste";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(2, 395);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(108, 88);
+            this.pictureBox2.TabIndex = 24;
+            this.pictureBox2.TabStop = false;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(116, 395);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(608, 88);
+            this.flowLayoutPanel2.TabIndex = 23;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(474, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 300);
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(2, 89);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(445, 300);
+            this.flowLayoutPanel1.TabIndex = 21;
+            // 
+            // Modulo6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 486);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Modulo6";
+            this.Text = "Modulo6";
+            this.Load += new System.EventHandler(this.Modulo6_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ Modulo6.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParcialPr
+{
+    public partial class Modulo6 : Form
+    {
+        char[] PalabrasAdivinadas; int oportunidades;
+        char[] PalabrasSeleccionada;
+        char[] Alfabeto;
+        String[] Palabras;
+        public Modulo6()
+        {
+            InitializeComponent();
+        }
+        public void IniciarJuego()
+        {
+            flowLayoutPanel1.Controls.Clear();
+            flowLayoutPanel1.Enabled = true;
+            pictureBox1.Image = null;
+            label1.Visible = false;
+            oportunidades = 0;
+
+            pictureBox1.Image = Properties.Resources.Jugando;
+            Palabras = new string[] { "MATERIAS", "UNIVERSIDAD", "ESTUDIANTES", "PROGRAMACION" };
+            Alfabeto = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ".ToCharArray();
+
+
+            Random random = new Random();
+            int IndicePalabra = random.Next(0, Palabras.Length);
+            PalabrasSeleccionada = Palabras[IndicePalabra].ToCharArray();
+            PalabrasAdivinadas = PalabrasSeleccionada;
+
+
+            foreach (char letraAl in Alfabeto)
+            {
+                Button btnLetra = new Button();
+                btnLetra.Tag = "";
+                btnLetra.Text = letraAl.ToString();
+                btnLetra.Width = 60;
+                btnLetra.Height = 40;
+                btnLetra.ForeColor = Color.White;
+                btnLetra.Font = new Font(btnLetra.Font.Name, 15, FontStyle.Bold);
+                btnLetra.BackgroundImageLayout = ImageLayout.Center;
+                btnLetra.BackColor = Color.Black;
+                btnLetra.Name = letraAl.ToString();
+                flowLayoutPanel1.Controls.Add(btnLetra);
+            }
+            flowLayoutPanel2.Controls.Clear();
+            for (int indicevalor = 0; indicevalor < PalabrasSeleccionada.Length; indicevalor++)
+            {
+                Button letra = new Button();
+                letra.Tag = PalabrasSeleccionada[indicevalor].ToString();
+                letra.Width = 60;
+                letra.Height = 40;
+                letra.ForeColor = Color.Purple;
+                letra.Text = "-";
+                letra.Font = new Font(letra.Font.Name, 32, FontStyle.Bold);
+                letra.BackgroundImageLayout = ImageLayout.Center;
+                letra.BackColor = Color.White;
+                letra.Name = "Adivinado" + letra.ToString();
+                letra.BackgroundImage = ((System.Drawing.Image)(Properties.Resources.acertijo));
+                flowLayoutPanel2.Controls.Add(letra);
+            }
+        }
+        void compara(object sender, EventArgs e)
+        {
+            bool encontrado = false;
+            Button btn = (Button)sender;
+            btn.BackColor = Color.White;
+            btn.ForeColor = Color.Black;
+            btn.Enabled = false;
+            for (int indicerevisar = 0; indicerevisar < PalabrasAdivinadas.Length; indicerevisar++)
+            {
+                if (PalabrasAdivinadas[indicerevisar] == char.Parse(btn.Text))
+                {
+                    Button tbx = this.Controls.Find("Adivinado" + indicerevisar, true).FirstOrDefault() as Button;
+                    tbx.Text = PalabrasAdivinadas[indicerevisar].ToString();
+                    PalabrasAdivinadas[indicerevisar] = '-';
+                    encontrado = true;
+                }
+            }
+            bool Ganaste = true;
+            for (int indiceganador = 0; indiceganador < PalabrasAdivinadas.Length; indiceganador++)
+            {
+                if (PalabrasAdivinadas[indiceganador] != '-')
+                {
+                    Ganaste = false;
+                }
+            }
+            if (Ganaste)
+            {
+                MessageBox.Show("Ganaste");
+                pictureBox3.Image = Properties.Resources.btnStart;
+            }
+            if (!encontrado)
+            {
+                oportunidades = oportunidades + 1;
+                pictureBox1.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("Ahorcado" + oportunidades);
+                if (oportunidades == 8)
+                {
+                    label2.Visible = true;
+                    for (int indicevalor = 0; indicevalor < PalabrasSeleccionada.Length; indicevalor++)
+                    {
+                        Button btnLetra = this.Controls.Find("Adivinado" + indicevalor, true).FirstOrDefault() as Button;
+                        btnLetra.Text = PalabrasSeleccionada[indicevalor].ToString();
+                    }
+                    flowLayoutPanel1.Enabled = false;
+                    pictureBox3.Image = Properties.Resources.btnStart;
+                }
+            }
+        }
+        private void Modulo6_Load(object sender, EventArgs e)
+        {
+            IniciarJuego();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            IniciarJuego();
+        }
+    }
+}
