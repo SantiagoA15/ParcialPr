11ae9bf95c6ea80b7026fc8aecb4692b3b9844bd
JHONATAN CAMILO SALINAS LOPEZ <jcamilosalinas@ucundinamarca.edu.co>
2021-09-24 09:16:56 -0500

    Taasd

---
+++ Moulo5.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace ParcialPr
+{
+    partial class Moulo5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(237, 327);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 19);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "Resultado";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(14, 205);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(440, 15);
+            this.label10.TabIndex = 46;
+            this.label10.Text = "Para la validación deberá optimizar el código haciendo validación por código asci" +
+    "i";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(14, 178);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(424, 15);
+            this.label9.TabIndex = 45;
+            this.label9.Text = "Tan pronto como se complete el panoramael programa deberámostrar un aviso.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 148);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(294, 15);
+            this.label8.TabIndex = 44;
+            this.label8.Text = "pero podrán aparecer en mayúsculas o en minúsculas.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 133);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(572, 15);
+            this.label7.TabIndex = 43;
+            this.label7.Text = "Sólo deberás considerar las letras del alfabeto inglés (no te preocupes por las t" +
+    "ildes, ni la eñe, entre otras), ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(14, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(683, 15);
+            this.label5.TabIndex = 42;
+            this.label5.Text = "Deberás crear un programaen el cual se estaráescribiendo una frasey deberáindicar" +
+    " elmomento en que la frase seaun pangrama. ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(499, 15);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "Son \"pangramas\",textos que contienen todas las letras de un cierto alfabeto, quiz" +
+    "á repetidas.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(178, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(196, 23);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "Digite la frase deseada";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(774, 15);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "Seguro que has visto textos como \"El veloz murciélago hindú comía feliz cardillo " +
+    "y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(327, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 18);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Modulo 5";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(689, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 37;
+            this.button3.Text = "Atras";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 383);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 36;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(221, 291);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 35;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 34;
+            this.button1.Text = "Ejecutar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Moulo5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Moulo5";
+            this.Text = "Moulo5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Moulo5.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParcialPr
+{
+    public partial class Moulo5 : Form
+    {
+        public Moulo5()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Texto = " ";
+            char[] Array;
+            List<String> datos = new List<string>();
+            string res = string.Empty;
+            Texto = textBox1.Text.Replace(" ", "");
+            int iguales = 32;
+            for (int j = 'A'; j <= 'z'; j++)//recorre los caracteres de la A la z en acsii
+            {
+                if (j < 91 || j > 96)//Omite los queno partene al abcedario entre Z y a
+                {
+                    string str = j.ToString();
+                    datos.Add(str);//Se añade a la lista los numeros del abecedario
+                }
+
+            }
+            for (int i = 0; i < Texto.Length; i++)// Se recorre el texto
+            {
+                int inp = Texto[i];// Se con vierte a entero
+                if (datos.Contains(inp.ToString()))//Mira si el numero correspondiente a su letra esta en la lista
+                {
+                    int igu = 0;
+                    if (inp < 91) //valida si la letra es mayuscula
+                    {
+                        igu = inp + iguales; //si es mayuscula, le suma 32 para borrar su semejante en minuscula
+                    }
+                    else
+                    {
+                        igu = inp - iguales; //si es mayor a 91, quiere decir que es minuscula y le resta 32 para borrar su semejante en mayuscula
+                    }
+                    datos.Remove(inp.ToString());// se remueve la letra correspondiente
+                    datos.Remove(igu.ToString());//Se remueve el semenjante en mayuscula o minuscula segun corresponda
+
+                }
+            }
+            if (datos.Count == 0)//Mira lo que sobra en la lista y lo compara si es igual a 0, la lista esta vacia por ende tiene todo el abc
+            {
+                label1.Text = ("Cumple");
+            }
+            else
+            {
+                label1.Text = ("No cumple");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
